1f50044d61538de6cf0286699438af3675d598c3
dukhyun lee <ideook@gmail.com>
2022-05-14 03:23:51 +0900

    MSSQL 로 변경

---
+++ SitAPI/Controllers/SitApiController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using Newtonsoft.Json;
+using System.Data;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.Text;
+using System.Web;
+using System;
+using Microsoft.Web.Administration;
+using System.Net;
+using System.Transactions;
+using MySql.Data.MySqlClient;
+
+namespace UnrealViewerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Sit : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private Transaction transaction = new Transaction();
+
+        public Sit(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("mldata")]
+        public string GetMlData()
+        {
+            string query = @"SELECT * FROM tbl_ml";
+
+            string dataSource = _configuration.GetConnectionString("MSSQLServerConnectionString");
+
+            return JsonConvert.SerializeObject(transaction.GetTableFromDB(query, dataSource));
+        }
+
+        [HttpGet]
+        [Route("tableinfo")]
+        public string GetProjects()
+        {
+            string query = @"SELECT name as field, label as headerName FROM tbl_def_cols";
+
+            string dataSource = _configuration.GetConnectionString("MSSQLServerConnectionString");
+
+            return JsonConvert.SerializeObject(transaction.GetTableFromDB(query, dataSource));
+        }
+    }
+}
---
+++ SitAPI/Controllers/Transaction.cs
@@ -0,0 +1,69 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace UnrealViewerAPI.Controllers
+{
+    public class Transaction
+    {
+        public DataTable GetTableFromDB(string query, string dataSource)
+        {
+            DataTable table = new DataTable();
+            SqlDataReader sqlDataReader;
+
+            using (SqlConnection sqlConnection = new SqlConnection(dataSource))
+            {
+                sqlConnection.Open();
+                using (SqlCommand sqlCommand = new SqlCommand(query, sqlConnection))
+                {
+                    sqlCommand.CommandTimeout = 60;
+                    sqlDataReader = sqlCommand.ExecuteReader();
+                    table.Load(sqlDataReader);
+                    sqlConnection.Close();
+                }
+            }
+
+            return table;
+        }
+        public Task<DataSet> GetDataSetAsync(string sConnectionString, string sSQL, int cur, int page)
+        {
+            return Task.Run(() =>
+            {
+                using (var newConnection = new SqlConnection(sConnectionString))
+                using (var mySQLAdapter = new SqlDataAdapter(sSQL, newConnection))
+                {
+                    mySQLAdapter.SelectCommand.CommandType = CommandType.Text;
+
+                    DataSet myDataSet = new DataSet();
+                    mySQLAdapter.Fill(myDataSet, cur, page, "TABLE");
+                    return myDataSet;
+                }
+            });
+        }
+        public DataSet GetPagingData(string query, string dataSource, string table, int currentIndex = 0, int pageSize = 5)
+        {
+            DataSet dataSet = new DataSet();
+
+            //int currentIndex = 0;
+            //int pageSize = 5;
+
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(dataSource))
+                {
+                    sqlConnection.Open();
+
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+
+                    sqlDataAdapter.Fill(dataSet, currentIndex, pageSize, table);
+
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            return dataSet;
+        }
+    }
+}
